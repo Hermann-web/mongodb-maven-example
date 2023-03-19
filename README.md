a6724c9e0a77d787729f04925b7450a2eadf4c55
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2023-03-19 12:10:57 +0000

    add option to connect with mongodb atlas

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 
 ### Connect to MongoDB in maven project using `mongodb-driver`. [Docs](https://mongodb.github.io/mongo-java-driver/3.2/driver/getting-started/installation-guide/)
+
+## Example of connection with mongo db 
+[github](https://github.com/roc41d/mongodb-maven-example)
+
+### connect to mongodb atlas
+[Docs](https://www.mongodb.com/docs/drivers/java/sync/v4.3/quick-start/) or [Docs](https://www.baeldung.com/java-mongodb)
+
+### Use netbeans
+[Starter](https://netbeans.apache.org/kb/docs/javaee/ecommerce/setup-dev-environ.html)
+
+
--- src/main/java/config/MongoConnectionManager.java
+++ src/main/java/config/MongoConnectionManager.java
@@ -12,23 +12,34 @@ public class MongoConnectionManager {
 
     private static final String MONGO_HOST = "localhost";
     private static final int MONGO_PORT = 27017;
+    
+    
+    private static final String atlasURI = "mongodb+srv://.....";
     public static MongoClient initConnection() {
-        List<ServerAddress> mongoHostList = new ArrayList<>();
-        mongoHostList.add(new ServerAddress(MONGO_HOST, MONGO_PORT));
-
+        boolean useAtlas = true; // set this to true or false depending on whether you want to use MongoDB Atlas or a local MongoDB instance
+        // // MongoDBHandler mongoDBHandler = new MongoDBHandler(useAtlas);
         try {
-            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                    .applyToClusterSettings(clusterSettingsBuilder -> clusterSettingsBuilder.hosts(mongoHostList))
-                    .writeConcern(WriteConcern.W1)
-                    .readConcern(ReadConcern.MAJORITY)
-                    .readPreference(ReadPreference.nearest())
-                    .retryWrites(true)
-                    .build();
-
-            MongoClient client = MongoClients.create(mongoClientSettings);
-
+            if (useAtlas) {
+            MongoClient client = MongoClients.create(atlasURI);
             return client;
+            }
+            else {
 
+                /*List<ServerAddress> mongoHostList = new ArrayList<>();
+                // mongoHostList.add(new ServerAddress(MONGO_HOST, MONGO_PORT));
+                mongoHostList.add(new ServerAddress(atlasURI));
+                MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                        .applyToClusterSettings(clusterSettingsBuilder -> clusterSettingsBuilder.hosts(mongoHostList))
+                        .writeConcern(WriteConcern.W1)
+                        .readConcern(ReadConcern.MAJORITY)
+                        .readPreference(ReadPreference.nearest())
+                        .retryWrites(true)
+                        .build();
+
+                MongoClient client = MongoClients.create(mongoClientSettings);*/
+                MongoClient client = MongoClients.create(atlasURI);
+                return client;
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return null;
