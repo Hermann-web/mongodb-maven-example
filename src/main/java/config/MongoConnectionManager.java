faa62f34a1320e4302de460d84854040f6b7003d
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2023-03-19 14:44:22 +0000

    Update MongoConnectionManager.java

--- src/main/java/config/MongoConnectionManager.java
+++ src/main/java/config/MongoConnectionManager.java
@@ -17,17 +17,15 @@ public class MongoConnectionManager {
     private static final String atlasURI = "mongodb+srv://.....";
     public static MongoClient initConnection() {
         boolean useAtlas = true; // set this to true or false depending on whether you want to use MongoDB Atlas or a local MongoDB instance
-        // // MongoDBHandler mongoDBHandler = new MongoDBHandler(useAtlas);
         try {
             if (useAtlas) {
-            MongoClient client = MongoClients.create(atlasURI);
-            return client;
+                MongoClient client = MongoClients.create(atlasURI);
+                return client;
             }
             else {
 
-                /*List<ServerAddress> mongoHostList = new ArrayList<>();
-                // mongoHostList.add(new ServerAddress(MONGO_HOST, MONGO_PORT));
-                mongoHostList.add(new ServerAddress(atlasURI));
+                List<ServerAddress> mongoHostList = new ArrayList<>();
+                mongoHostList.add(new ServerAddress(MONGO_HOST, MONGO_PORT));
                 MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                         .applyToClusterSettings(clusterSettingsBuilder -> clusterSettingsBuilder.hosts(mongoHostList))
                         .writeConcern(WriteConcern.W1)
@@ -36,8 +34,7 @@ public class MongoConnectionManager {
                         .retryWrites(true)
                         .build();
 
-                MongoClient client = MongoClients.create(mongoClientSettings);*/
-                MongoClient client = MongoClients.create(atlasURI);
+                MongoClient client = MongoClients.create(mongoClientSettings);
                 return client;
             }
         } catch (Exception e) {
